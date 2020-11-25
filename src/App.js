66752476c860d605849135fe2faaab7c15f33fd2
Dan <dannbulgarelli@gmail.com>
2020-11-25 11:09:33 -0600

    put async api call in function

--- src/App.js
+++ src/App.js
@@ -13,65 +13,27 @@ export default class App extends React.Component {
     bills: "r",
   };
 
-  getBills = () => {
-    return new Promise((resolve, reject) => {
-      fetch(url, {
-        method: "GET",
-        headers: {
-          "X-API-Key": "ewzo1NlgyHsISCfzkgVniHyLBCvN4JtjpXWsDKks",
-        },
-      });
-    });
-  };
+  componentDidMount() {
+    this.callApi(url);
+  }
 
-  async componentDidMount() {
-    let response = await fetch(url, {
+  async callApi(apiUrl) {
+    let response = await fetch(apiUrl, {
       method: "GET",
       headers: {
         "X-API-Key": "ewzo1NlgyHsISCfzkgVniHyLBCvN4JtjpXWsDKks",
       },
     });
     json = await response.json();
-    //let json = await console.log(response.json());
-    let bills = await json.results[0];
-    console.log(bills);
-    debugger;
-    //need await and async
-    //Don't think that fetch is working correctly
-
-    //}).then((res) => this.setState(() => ({ bills: res })));
-
-    //send this to bills
-    //console.log("bills state" + this.state.bills);
+    let bills = await json.results[0].bills;
+    //parse for errors
+    //update state with await function
   }
 
-  // async function callApi(url) {
-  //   try {
-  //     await fetch(url, {
-  //       method: "GET",
-  //       headers: {
-  //         "X-API-Key": "ewzo1NlgyHsISCfzkgVniHyLBCvN4JtjpXWsDKks",
-  //       },
-  //     })
-  //       .then((resp) => resp.json())
-  //       .then(function (data) {
-  //         //console.log(data);
-  //         return data;
-  //       });
-  //   } catch {
-  //     console.log("error");
-  //   }
-  // }
-
   componentDidUpdate() {
-    //let parsedData = JSON.parse(this.state.bills);
     console.log(this.state.bills);
   }
 
-  hi = () => {
-    //where we will put the api info
-    console.log("hi");
-  };
   render() {
     return (
       <div className="App">
